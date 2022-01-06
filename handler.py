d5964f1c0ce29dddefb297bcc3c8e47062aab633
dallin <dallinwright01@gmail.com>
2022-01-06 15:18:41 +0100

    bootstrapping

---
+++ handler.py
@@ -0,0 +1,19 @@
+import json
+
+
+def main(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+
+if __name__ == "__main__":
+    main(None, None)
