f952695ed0893b8fd835e83277a698ae102e74e2
dallin <dallinwright01@gmail.com>
2022-01-07 17:40:12 +0100

    updating

--- modules/config/load_config.py
+++ modules/config/load_config.py
@@ -1,7 +1,17 @@
 import os
 
+from modules.config.logger import logger
+from modules.get_secret import get_secret
+
 
 def load_config():
+    aws_secret_name = os.environ['SECRET_NAME']
+    aws_region = os.environ['AWS_REGION']
+
+    secret = get_secret(aws_secret_name, aws_region)
+
+    logger.info(secret)
+
     config_dict = {
         "oracle_app_id": os.environ['ORACLE_APP_ID'],
         "admin_wallet_address": os.environ['ADMIN_WALLET_ADDRESS'],
