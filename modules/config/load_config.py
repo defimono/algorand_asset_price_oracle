cdb8159ca46f7bc91f43663ae41a958fe5262540
dallin <dallinwright01@gmail.com>
2022-01-07 17:50:40 +0100

    working aws integration, ready for lambda

--- modules/config/load_config.py
+++ modules/config/load_config.py
@@ -1,22 +1,27 @@
+import json
 import os
 
 from modules.config.logger import logger
 from modules.get_secret import get_secret
 
+import os
+os.environ["AWS_SHARED_CREDENTIALS_FILE"] = "/home/dallin/.aws/credentials"
+os.environ["AWS_DEFAULT_PROFILE"] = "ss-dev"
 
 def load_config():
     aws_secret_name = os.environ['SECRET_NAME']
     aws_region = os.environ['AWS_REGION']
 
-    secret = get_secret(aws_secret_name, aws_region)
+    secret_string = get_secret(aws_secret_name, aws_region)
+    secret = json.loads(secret_string)
 
     logger.info(secret)
 
     config_dict = {
         "oracle_app_id": os.environ['ORACLE_APP_ID'],
-        "admin_wallet_address": os.environ['ADMIN_WALLET_ADDRESS'],
-        "admin_private_key": os.environ['ADMIN_PRIVATE_KEY'],
-        "admin_mnemonic": os.environ['ADMIN_MNEMONIC'],
+        "admin_wallet_address": secret.get("ADMIN_WALLET_ADDRESS"),
+        "admin_private_key": secret.get("ADMIN_PRIVATE_KEY"),
+        "admin_mnemonic": secret.get("ADMIN_WALLET_ADDRESS"),
         "algod_address": os.environ['ALGOD_ADDRESS'],
         "algod_token": os.environ['ALGOD_KEY'],
         "algod_chain": os.environ['ALGOD_CHAIN'],
